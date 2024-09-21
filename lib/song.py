c294d3aa34815cfb22f3f67d7bd6835be363c385
kacha930 <robiwere20@gmail.com>
2024-09-21 07:39:39 +0300

    complete lab -song

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,67 @@
+
+
+
 class Song:
-    pass
+    # class attributes - it is the work of the class to know how many tracks are there.
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count={}
+    
+    def __init__(self, name, artist, genre):
+        self.add_song_to_count()
+
+        #  adds any new genres to a class attribute genres, a list. This list should contain only unique genres — no duplicates! 
+        self.add_to_genres(genre)
+
+
+        self.add_to_artists(artist)
+
+        # adds to a class attribute genre_count, a dictionary in which the keys are the names of each genre. Each genre name key should point to a value that is the number of songs that have that genre.
+
+
+        self.add_to_genre_count(genre)
+
+        # creates a histogram similar to the one above, but for artists rather than genres.
+
+
+        self.add_to_artist_count(artist)
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+    
+
+    @classmethod
+    def add_song_to_count(cls, increment = 1):
+        cls.count += increment
+        
+    
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+        
+    
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+        
+    
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] +=1
+        else:
+            cls.genre_count[genre] =1 
+        
+    
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist]  = 1  
+        
+
